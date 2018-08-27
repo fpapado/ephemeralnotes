e92d5482367f0d668f749c47de6463dde85254ad
fpapado <fotis@futurice.com>
2018-08-28 02:27:23 +0300

    Remove old importScripts

--- src/sw.js
+++ src/sw.js
@@ -1,8 +1,2 @@
-importScripts("/third_party/workbox/workbox-sw.js");
-
-workbox.setConfig({
-  modulePathPrefix: "/third_party/workbox/"
-});
-
 workbox.precaching.precacheAndRoute(self.__precacheManifest || []);
 workbox.routing.registerNavigationRoute("/");
