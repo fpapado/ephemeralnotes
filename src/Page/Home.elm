1dc10b70aa1c787e1d85685261232b7bf9868309
fpapado <fotis@futurice.com>
2019-04-01 01:09:56 +0300

    Initial form and outline

--- src/Page/Home.elm
+++ src/Page/Home.elm
@@ -15,8 +15,8 @@ import Asset
 import Entry.Entry as Entry exposing (Entry)
 import Geolocation as Geo
 import Html exposing (..)
-import Html.Attributes exposing (class, href)
-import Html.Events exposing (onClick)
+import Html.Attributes as HA exposing (class, href)
+import Html.Events as HE exposing (onClick)
 import Location as L
 import RemoteData exposing (RemoteData)
 import ServiceWorker as SW
@@ -36,6 +36,22 @@ type alias Model =
     , installPrompt : SW.InstallPrompt
     , location : GeolocationData
     , entries : RemoteData String (List Entry)
+    , form : Form
+    }
+
+
+type alias Form =
+    { front : String
+    , back : String
+    , saveLocation : Bool
+    }
+
+
+emptyForm : Form
+emptyForm =
+    { front = ""
+    , back = ""
+    , saveLocation = True
     }
 
 
@@ -56,6 +72,7 @@ init =
       , installPrompt = SW.installPromptNone
       , location = NotAsked
       , entries = RemoteData.Loading
+      , form = emptyForm
       }
     , Cmd.batch
         [ Task.perform GotTimeZone Time.here
@@ -83,14 +100,77 @@ viewInner model =
     div []
         [ Ui.centeredContainer
             []
-            [ div [ class "vs4" ]
-                [ div [ class "vs3" ]
+            [ div [ class "vs4 vs5-ns" ]
+                [ viewForm model.form
+                , div [ class "vs3 vs4-ns" ]
                     [ subHeading 2 [] [ text "Entries" ]
                     , viewEntries model.entries
                     ]
                 ]
             , viewUpdatePrompt model.swUpdate
             , viewInstallPrompt model.installPrompt
+            ]
+        ]
+
+
+viewForm : Form -> Html Msg
+viewForm form_ =
+    form [ class "vs3 vs4-ns", HE.onSubmit FormSubmitClicked, HA.autocomplete False ]
+        [ subHeading 2 [ class "decor" ] [ text "Add Entry" ]
+        , div [ class "vs3 vs4-ns mw6 center" ]
+            [ div [ class "vs2" ]
+                [ label
+                    [ class "db fw6 f5 f4-ns"
+                    , HA.for "entry-front"
+                    ]
+                    [ text "Front" ]
+                , input
+                    [ class "mw6 w-100 db pa2 fw4 f5 f4-ns ba bw1 b--near-black br1 focus-shadow-light"
+                    , HA.type_ "text"
+                    , HA.id "entry-front"
+                    , HA.name "front"
+                    , HE.onInput FormFrontChanged
+                    , HA.value form_.front
+                    , HA.placeholder "Ephemeral"
+                    ]
+                    []
+                ]
+            , div [ class "vs2" ]
+                [ label
+                    [ class "db fw6 f5 f4-ns"
+                    , HA.for "entry-back"
+                    ]
+                    [ text "Back" ]
+                , input
+                    [ class "mw6 w-100 db pa2 fw4 f5 f4-ns ba bw1 b--near-black br1 focus-shadow-light"
+                    , HA.type_ "text"
+                    , HA.id "entry-back"
+                    , HA.name "back"
+                    , HE.onInput FormBackChanged
+                    , HA.value form_.back
+                    , HA.placeholder "Lasting a short time, fleeting"
+                    ]
+                    []
+                ]
+            , div [ class "flex" ]
+                [ input
+                    [ class "db mr2 f5 f4-ns"
+                    , HA.type_ "checkbox"
+                    , HA.id "entry-location"
+                    , HA.name "save location"
+                    , HE.onCheck FormLocationToggled
+                    , HA.checked form_.saveLocation
+                    ]
+                    []
+                , label
+                    [ class "db fw6 f5 f4-ns"
+                    , HA.for "entry-location"
+                    ]
+                    [ text "Save location" ]
+                ]
+            , styledButtonBlue []
+                [ text "Save entry"
+                ]
             ]
         ]
 
@@ -119,7 +199,7 @@ viewEntryList entryList =
         [] ->
             div
                 [ class "vs3 tc" ]
-                [ div [ class "mw4 mw5-l center" ]
+                [ div [ class "mw5 center" ]
                     [ div [ class "aspect-ratio aspect-ratio--1x1" ]
                         [ img (class "aspect-ratio--object db" :: Asset.toAttr Asset.noData) []
                         ]
@@ -232,6 +312,11 @@ type Msg
     | AcceptInstallPrompt
     | DeferInstallPrompt
     | GetLocation
+    | FormSubmitClicked
+    | FormSubmittedOk
+    | FormFrontChanged String
+    | FormBackChanged String
+    | FormLocationToggled Bool
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -286,6 +371,55 @@ update msg model =
 
         GetLocation ->
             ( model, Geo.getLocation )
+
+        -- Form
+        FormFrontChanged front ->
+            let
+                form =
+                    model.form
+
+                newForm =
+                    { form | front = front }
+            in
+            ( { model | form = newForm }, Cmd.none )
+
+        FormBackChanged back ->
+            let
+                form =
+                    model.form
+
+                newForm =
+                    { form | back = back }
+            in
+            ( { model | form = newForm }, Cmd.none )
+
+        FormLocationToggled toggle ->
+            let
+                form =
+                    model.form
+
+                newForm =
+                    { form | saveLocation = toggle }
+            in
+            ( { model | form = newForm }, Cmd.none )
+
+        -- TODO: On Form submit:
+        -- - get location if specified
+        -- - tie that to the form somehow / prevent the form from being edited and the button clicked
+        -- - add a "loading" to the button
+        -- - if failed, show message
+        -- - if succeeded, send to JS side for storing
+        -- - keep "loading" with "saving"
+        -- - if succeeded, reset form
+        -- - if failed, show message, do not reset
+        -- NOTE: need a way to relate the GotResult/SavedOk to the form submit, maybe through some key?
+        -- NOTE: Need a way to encode partials, e.g Entry.encodePartial
+        -- NOTE: Check JS-side for the uuid generation
+        FormSubmitClicked ->
+            ( model, Cmd.none )
+
+        FormSubmittedOk ->
+            ( { model | form = emptyForm }, Cmd.none )
 
 
 
