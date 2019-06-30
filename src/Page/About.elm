7c06f2d948bcaf255d3bdbc002d34c8b66b86c24
fpapado <fotis@futurice.com>
2019-07-01 00:13:27 +0300

    Add donation link

--- src/Page/About.elm
+++ src/Page/About.elm
@@ -68,7 +68,8 @@ viewFaq =
             ]
         , div [ class "vs3" ]
             [ subHeading 3 [] [ text "Can I donate to the development?" ]
-            , paragraph [] [ text "If you have used this app and found it useful, you can donate through buy me a coffee." ]
+            , paragraph [] [ text "If you have used this app and found it useful, you can donate through " ]
+            , a [ href "https://www.buymeacoffee.com/isfotis", class "color-accent" ] [ text "buy me a coffee." ]
             , paragraph []
                 [ text "To be fully transparent: I am partially compensated for my work through the "
                 , a [ href "https://spiceprogram.org/", class "color-accent" ] [ text "Chillicorn open source program" ]
