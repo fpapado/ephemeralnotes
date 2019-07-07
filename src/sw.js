18a305021009b3ee6ac64d034b872b25d008c458
fpapado <fotis@futurice.com>
2019-07-07 14:16:49 +0300

    Cache fonts at runtime

--- src/sw.js
+++ src/sw.js
@@ -16,3 +16,17 @@ self.addEventListener('message', messageEvent => {
       break;
   }
 });
+
+// Cache webfonts at runtime
+workbox.routing.registerRoute(
+  /\.(?:woff|woff2)$/,
+  new workbox.strategies.CacheFirst({
+    cacheName: 'fonts',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxAgeSeconds: 60 * 60 * 24 * 365, // 1 year
+        maxEntries: 30,
+      }),
+    ],
+  })
+);
