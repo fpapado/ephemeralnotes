89689ec2529b1b4f4bf961426e5a25710590292b
Fotis Papadogeorgopoulos <fotis@futurice.com>
2019-07-11 00:31:22 +0300

    Use the correct invocation for navigation route (#35)

--- src/sw.js
+++ src/sw.js
@@ -1,5 +1,9 @@
 workbox.precaching.precacheAndRoute(self.__precacheManifest || []);
-workbox.routing.registerNavigationRoute('/');
+workbox.routing.registerNavigationRoute(
+  // Assuming '/index.html' has been precached,
+  // look up its corresponding cache key.
+  workbox.precaching.getCacheKeyForURL('/index.html')
+);
 
 // Listen for postMessage(), well, mesages
 self.addEventListener('message', messageEvent => {
