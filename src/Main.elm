5b1a7b33428c82caa7a13e39efb8882bb4e63df5
fpapado <fotis@futurice.com>
2019-07-13 17:07:40 +0300

    Try .sleep 200 vs 100

--- src/Main.elm
+++ src/Main.elm
@@ -243,7 +243,7 @@ page loads to Assistive Technology users could be considered.
 -}
 delayedFocus : String -> Task Dom.Error ()
 delayedFocus id =
-    Process.sleep 100
+    Process.sleep 200
         |> Task.andThen (\() -> Dom.focus id)
 
 
